1f8f22d9d2dbbff05d947bb2a8452ad457a8d2f3
JC21-230023 <230023@jc-21.jp>
2025-12-03 10:57:36 +0900

    cursorさんに確認された

--- src/main/java/jp/co/jc21ps/activity_management/repository/JoinApprovalRepository.java
+++ src/main/java/jp/co/jc21ps/activity_management/repository/JoinApprovalRepository.java
@@ -22,7 +22,24 @@ public class JoinApprovalRepository {
          * TODO ➊ 初期表示情報を取得するSQLを完成させる。
          */
         String sql = """
+                        select
+                            j_requ.club_id,
+                            j_requ.user_id,
+                            club.club_name,
+                            user.user_name
 
+                        FROM
+                            trn_join_request AS j_requ
+                        JOIN
+                            mst_user AS user 
+                            ON  j_requ.user_id= user.user_id
+                        
+                       JOIN
+                            mst_club AS club 
+                            ON  j_requ.club_id= club.club_id
+
+                        WHERE
+                            j_requ.user_id=?
                 """;
 
         List<Map<String, Object>> joinApprovalList = jdbcTemplate.queryForList(sql, paramEntity.getClubId());
@@ -77,14 +94,25 @@ public class JoinApprovalRepository {
          * TODO ➋ ユーザーを承認するSQL文を完成させる。
          */
         String sqlInsert = """
-
+                      INSERT INTO
+                        trn_club_member(
+                            user_id,
+                            club_Id,
+                            leader_flg
+                        )
+                        SELECT 
+                            user_id,
+                            club_id,
+                            leader_flg
+                        FROM 
+                            trn_join_request
+                        WHERE 
+                            user_id=?
                 """;
 
         // entityから値をゲット
         Object[] paramList = {
-                paramEntity.getClubId(),
-                paramEntity.getUserId(),
-                paramEntity.isLeaderFlg()
+                paramEntity.getUserId()
         };
 
         jdbcTemplate.update(sqlInsert, paramList);
@@ -96,7 +124,11 @@ public class JoinApprovalRepository {
          * TODO ➌ ユーザーを否認するSQL文を完成させる。
          */
         String sqlDelete = """
-
+                        DELETE
+                        FROM
+                            trn_join_request
+                        WHERE
+                            user_id=?
                 """;
 
         // entityから値をゲット
